35716edb3e002f73ab3b8694fcf0bfdae9a52b6c
im-sudhir <ssudhir794@gmail.com>
2024-12-28 01:45:11 +0530

    added serializer and model classes

--- nsapp/views.py
+++ nsapp/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from rest_framework import generics
+from .serializers import *
+from .models import *
 # Create your views here.
+
+class InstructorListView(generics.ListCreateAPIView):
+    serializer_class= InstructorSerializer
+    queryset= Instructor.objects.all()
+
+class InstructorDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class=InstructorSerializer
+    queryset=Instructor.objects.all()
+
+class CourseListView(generics.ListCreateAPIView):
+    serializer_class= CourseSerializer
+    queryset= Course.objects.all()
