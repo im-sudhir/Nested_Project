5c649412d1541facfbcb1a09bdd4109fb4a5c9da
im-sudhir <ssudhir794@gmail.com>
2024-12-28 21:44:01 +0530

    Created a custom permission to check that post request can only be made by admin.

--- nsapp/views.py
+++ nsapp/views.py
@@ -2,9 +2,24 @@ from django.shortcuts import render
 from rest_framework import generics
 from .serializers import *
 from .models import *
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, BasePermission
+from rest_framework.authentication import BasicAuthentication
+
+
+class WriteByAdminOnly(BasePermission):
+    def has_permission(self, request, view):
+        user=request.user
+        if request.method=='GET':
+            return True
+        if request.method=='POST' or request.method=='PUT' or request.method=='DELETE':
+            if user.is_superuser:
+                return True
+        return False
+        
 
 # Instructor Views
 class InstructorListView(generics.ListCreateAPIView):
+    permission_classes=[IsAuthenticated]
     serializer_class = InstructorSerializer
     queryset = Instructor.objects.all()
 
@@ -14,8 +29,11 @@ class InstructorDetailView(generics.RetrieveUpdateDestroyAPIView):
 
 # Course Views
 class CourseListView(generics.ListCreateAPIView):
+    authentication_classes=[BasicAuthentication]
+    permission_classes=[IsAuthenticated,WriteByAdminOnly]
     serializer_class = CourseSerializer
     queryset = Course.objects.all()
+
 
 class CourseDetailView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = CourseSerializer
